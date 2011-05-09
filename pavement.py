0e0dd17c210e07fe6de2fc5d10482a61fc4758dc
Travis J Parker <travis.parker@gmail.com>
2011-05-09 12:08:20 -0700

    beta quality
    Change-Id: I0810df90dd54f3797b22cbdfc5975f8fd965fbb0

--- pavement.py
+++ pavement.py
@@ -23,7 +23,7 @@ setup(
         include_dirs=('.',),
         extra_compile_args=['-Wall'])],
     classifiers = [
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
