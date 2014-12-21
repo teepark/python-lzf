3f523c5029c91b75afc09cead9900a63fb311694
Travis J Parker <travis.parker@gmail.com>
2014-12-21 12:52:44 -0800

    ditch paver, vanilla setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,27 @@
-import os
-if os.path.exists("paver-minilib.zip"):
-    import sys
-    sys.path.insert(0, "paver-minilib.zip")
+from setuptools import setup, Extension
 
-import paver.tasks
-paver.tasks.main()
+
+VERSION = (0, 2, 1)
+
+setup(
+    name="python-lzf",
+    description="C Extension for liblzf",
+    version=".".join(filter(None, map(str, VERSION))),
+    author="Travis Parker",
+    author_email="travis.parker@gmail.com",
+    url="http://github.com/teepark/python-lzf",
+    license="BSD",
+    ext_modules=[Extension(
+        'lzf',
+        ['lzf_module.c', 'lzf_c.c', 'lzf_d.c'],
+        include_dirs=('.',),
+        extra_compile_args=['-Wall'])],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: C",
+        "Topic :: System :: Archiving :: Compression",
+    ]
+)
